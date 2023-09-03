d086ce008865df68ad931124051316c06d0147ed
concept <conceptcodes@gmail.com>
2023-09-03 12:42:42 -0400

    🔧 refactor(mail.ts): improve code readability and maintainability by introducing types and separating template loading logic into a separate function ✨ feat(mail.ts): add support for dynamic email templates with configurable subject and component functions 🔧 refactor(mail.ts): improve sendEmail function by extracting subject and html variables for better readability and maintainability ✨ feat(mail.ts): add checkEmailHealth function to check the health of the email service provider

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -5,41 +5,68 @@ import fs from "fs";
 
 const resend = new Resend(env.RESEND_API_KEY);
 
-const getHtml = (name: string, data?: Record<string, unknown>) => {
+type Data = Record<string, string>;
+
+type EmailTemplates = {
+  [key: string]: {
+    subject: string;
+    component: (data: Data) => string;
+  };
+};
+
+const loadTemplate = (name: string, args: Record<string, string>): string => {
   const template = fs.readFileSync(
     path.join(__dirname, "..", "emails", `${name}.html`),
     "utf8"
   );
   return template.replace(/{{([^{}]*)}}/g, (a, b) => {
-    const r = data[b];
-    return typeof r === "string" || typeof r === "number" ? r : a;
+    const value = args[b];
+    return typeof value === "string" ? value : a;
   });
 };
 
-const templates = {
+const templates: EmailTemplates = {
   welcome: {
     subject: "Welcome to Acme",
-    component: getHtml("welcome"),
+    component: (data) => {
+      return loadTemplate("welcome", data);
+    },
   },
   resetPassword: {
     subject: "Reset your password",
-    component: getHtml("reset-password"),
+    component: (data) => {
+      return loadTemplate("reset-password", data);
+    },
   },
 };
 
-export async function sendEmail(
+export async function sendEmail<T extends keyof typeof templates>(
   email: string,
-  template: keyof typeof templates
+  template: T,
+  data: Data
 ) {
   try {
+    const subject = templates[template].subject;
+    const html = templates[template].component(data);
+
     await resend.emails.send({
       from: "Zoomies <support@zoomies.dev>",
       to: [email],
-      subject: templates[template].subject,
-      html: templates[template].component,
+      subject,
+      html,
     });
   } catch (error) {
     console.error(error);
     throw error;
+  }
+}
+
+export async function checkEmailHealth() {
+  try {
+    const { data } = await resend.domains.list();
+    return data.length > 0;
+  } catch (error) {
+    console.error(error);
+    return false;
   }
 }
