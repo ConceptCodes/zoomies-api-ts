94f0a2b0af5d9d3b74081f698c3eeb1287eb0872
concept <conceptcodes@gmail.com>
2023-09-03 12:19:03 -0400

    🔀 refactor(enums.ts): rename PLANET_SCALE enum value to DATABASE for better clarity and semantics
    🔀 refactor(interface.ts): change isConnected property to connected and update service property type to keyof typeof Services for better type safety
    
    🔀 refactor(db/index.ts): add checkDatabaseHealth function to check the health of the database connection
    
    🔀 refactor(health.ts): add checkDatabaseHealth and checkEmailHealth functions to check the health of the database and email services respectively. Update checkIntegrationsHealth function to use these new functions and return the health report.

--- src/services/health.ts
+++ src/services/health.ts
@@ -1,4 +1,6 @@
 import { IHealthStatus } from "@/constants";
+import { checkDatabaseHealth } from "@/lib/db";
+import { checkEmailHealth } from "@/lib/mail";
 
 export default class HealthService {
   private healthReport: IHealthStatus[];
@@ -8,7 +10,20 @@ export default class HealthService {
   }
 
   public async checkIntegrationsHealth(): Promise<IHealthStatus[]> {
-    this.healthReport = [];
+    this.healthReport = await Promise.all([
+      this.checkDatabaseHealth(),
+      this.checkEmailHealth(),
+    ]);
     return this.healthReport;
+  }
+
+  public async checkDatabaseHealth(): Promise<IHealthStatus> {
+    const connected = await checkDatabaseHealth();
+    return { service: "DATABASE", connected };
+  }
+
+  public async checkEmailHealth(): Promise<IHealthStatus> {
+    const connected = await checkEmailHealth();
+    return { service: "RESEND", connected };
   }
 }
